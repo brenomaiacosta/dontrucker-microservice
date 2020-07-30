3fa271bac07b5f9bc8cfff4d812d6ea9d2f587d3
Henrique Basoni <hgbasoni@gmail.com>
2020-07-29 23:21:39 -0300

    Commit 29/07/2020 11:21.

---
+++ dt-offer-service/src/main/java/com/dontrucker/offer/DtOfferServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dontrucker.offer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtOfferServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtOfferServiceApplication.class, args);
+	}
+
+}
---
+++ dt-offer-service/src/test/java/com/dontrucker/offer/DtOfferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dontrucker.offer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtOfferServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
