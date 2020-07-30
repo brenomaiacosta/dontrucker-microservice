3140b271318226b372ecdb36589dac5beaeba229
Henrique Basoni <hgbasoni@gmail.com>
2020-07-29 23:39:51 -0300

    Commit 29/07/2020 11:39.

---
+++ dt-user-service/src/main/java/com/dontrucker/user/DtUserServiceApplication.java
@@ -0,0 +1,15 @@
+package com.dontrucker.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class DtUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtUserServiceApplication.class, args);
+	}
+
+}
---
+++ dt-user-service/src/test/java/com/dontrucker/user/DtUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dontrucker.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
