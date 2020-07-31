d0c0864e5ad72da4f843e4c7035f06cc41ea873b
Henrique Basoni <hgbasoni@gmail.com>
2020-07-31 01:13:02 -0300

    Commit 31/07/2020 01:12.

--- dt-user-service/src/main/java/com/dontrucker/user/repository/UserRepository.java
+++ dt-user-service/src/main/java/com/dontrucker/user/repository/UserRepository.java
@@ -2,8 +2,11 @@ package com.dontrucker.user.repository;
 
 import com.dontrucker.user.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
+    @Query()
+    User findByUsernameAndPassword(String username, String password);
 }
